a4653d09b35331f8babaf45f5975f368218223cc
Ross Cairns <ross@electricglen.com>
2013-10-03 16:34:52 +0100

    Added decay shader map util

--- addons/WebSystem/src/Utils.h
+++ addons/WebSystem/src/Utils.h
@@ -7,6 +7,7 @@
 namespace WebSystem {
     namespace Utils {
         
+        // Returns the number of hasg tags in a JSON array
         static const int countHashTagsInResult(Json::Value result) {
             if ( !result.isArray() ) {
                 ofLogError() << "countHashTagsInResult passed result which is not array";
@@ -22,6 +23,7 @@ namespace WebSystem {
             return total;
         }
         
+        // Applies a payload to the effects
         static const void applyPayload( VideoFX *videoFX, Json::Value payload ) {
             for (int i = 0;  i<payload["effects"].size(); ++i) {
                 for (vector<BaseEffect*>::iterator it = videoFX->effects.begin(); it!= videoFX->effects.end(); ++it) {
@@ -31,6 +33,35 @@ namespace WebSystem {
                     }
                 }
             }
+        }
+        
+        // Decays effect
+        static const void decayEffects( VideoFX *videoFX, float lastDecayTime, float decayRatePerSecondScalar ) {
+            
+            float timeDelta = ofGetElapsedTimef() - lastDecayTime;
+
+            for (vector<BaseEffect*>::iterator it = videoFX->effects.begin(); it!= videoFX->effects.end(); ++it) {
+             
+                Json::Value currentShaderMap = (*it)->getShaderMap();
+                Json::Value defaultShaderMap = (*it)->getShaderDefaultMap();
+
+                Json::Value::Members members = currentShaderMap.getMemberNames();
+                for ( Json::Value::Members::iterator vit = members.begin();  vit != members.end(); ++vit ) {
+
+                    float currentValue = currentShaderMap[ *vit ].asFloat();
+                    float defaultValue = defaultShaderMap[ *vit ].asFloat();
+                    float diff =  currentValue - defaultValue;
+
+                    if ( diff > 0.0001 || diff > -0.001) {
+                        float decayAmount = diff * decayRatePerSecondScalar * timeDelta;
+                        currentShaderMap[ *vit ] = currentValue - decayAmount;
+                    }
+
+                }
+
+                (*it)->applyShaderMap(currentShaderMap);
+            }
+            
         }
         
     }
