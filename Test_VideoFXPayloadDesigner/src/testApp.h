69dd154058320365b3f2eb203eb038891b5312d8
Ross Cairns <ross@electricglen.com>
2013-10-04 11:29:16 +0100

    Adding Designer

---
+++ Test_VideoFXPayloadDesigner/src/testApp.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxCv.h"
+#include "ofxUI.h"
+#include "FarnebackPlus.h"
+#include "CircularTexture.h"
+#include "KhronosEffect.h"
+#include "VideoFX.h"
+#include "VideoFXExporter.h"
+
+class testApp : public ofBaseApp{
+
+	public:
+    
+        int w, h;
+    
+    ofxFluid fluid;
+    
+        ofColor colors[7] = {
+            ofColor::red,
+            ofColor::orange,
+            ofColor::yellow,
+            ofColor::green,
+            ofColor::blue,
+            ofColor::indigo,
+            ofColor::violet
+        };
+        int colorIndex = 0;
+    
+        ofVideoGrabber videoGrabber;
+        ofVideoPlayer videoPlayer;
+        ofBaseVideoDraws *videoSource;
+    
+        VideoFX vfx1, vfx2;
+        VideoFX *big;
+        VideoFX *small;
+        ofxUICanvas *videoGUI;
+    
+		void setup();
+        void setupGUI();
+		void update();
+		void draw();
+        void guiEvent(ofxUIEventArgs &e);
+        void exit();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		
+        VideoFXExporter videoFXExporter;    
+};
