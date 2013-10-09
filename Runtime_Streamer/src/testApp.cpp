bed28a8e820e8cfbc5749acc01701ec26feb4eca
Ross Cairns <r@theworkers.net>
2013-10-09 12:02:32 +0100

    Added Runtime_Streamer project

---
+++ Runtime_Streamer/src/testApp.cpp
@@ -0,0 +1,112 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+    ofEnableAntiAliasing();
+    ofSetVerticalSync( true );
+    ofSetFrameRate( 30 );
+    
+
+    ofSetLogLevel( OF_LOG_WARNING );
+    w = 1280;
+    h = 720;
+    int flowW = w/4;
+    int flowH = h/4;
+    
+//    videoGrabber.setDeviceID( 1 );
+    videoGrabber.initGrabber( 640, 480 );
+    videoSource = &videoGrabber;
+    
+    vfx1.init();
+    vfx1.setVideoSource( &videoGrabber );
+    
+    vfx1.setColor( colors[colorIndex] );
+    
+    big = &vfx1;
+    
+    videoFXExporter.setVideoFX( &vfx1 );
+    
+    
+    overlay.init();
+}
+
+
+//--------------------------------------------------------------
+void testApp::update(){
+    
+    videoGrabber.update();
+    
+    vfx1.update( videoGrabber.isFrameNew() );
+    
+    overlay.update();
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+    
+    big->draw( ofGetWidth(), 0, -ofGetWidth(), ofGetHeight() );
+    
+    overlay.draw();
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key) {
+    
+    if ( key == 91 ) {
+        overlay.voteDisplay.addVote( overlay.voteDisplay.topic1 );
+    }
+    else if ( key == 93 ) {
+        overlay.voteDisplay.addVote( overlay.voteDisplay.topic2 );
+    }
+    
+    if ( key == 'k' ) {
+        vfx1.reloadShaders();
+    }
+    else if ( key == 161 ) { // tilda key
+        big->hideGUI();
+        videoFXExporter.exporterGUI->setVisible( false );
+        overlay.overlayGUI->setVisible( false );
+    }
+    else if ( key == '1' ) {
+        big = &vfx1;
+        big->showGUI();
+        videoFXExporter.exporterGUI->setVisible( true );
+        overlay.overlayGUI->setVisible( true );
+    }
+    else if ( key == 45 ) {
+        if ( --colorIndex < 0 )
+            colorIndex = 6;
+        vfx1.setColor( colors[colorIndex] );
+    }
+    else if ( key == 61 ) {
+        if ( ++colorIndex > 6 )
+            colorIndex = 0;
+        vfx1.setColor( colors[colorIndex] );
+    }
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){
+    
+}
+
+void testApp::exit() {
+    vfx1.exit();
+    videoFXExporter.exporterGUI->saveSettings("GUI/exporter.xml");
+    overlay.overlayGUI->saveSettings( "GUI/overlay.xml" );
+}
---
+++ Runtime_Streamer/src/testApp.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxCv.h"
+#include "ofxUI.h"
+#include "FarnebackPlus.h"
+#include "CircularTexture.h"
+#include "KhronosEffect.h"
+#include "VideoFX.h"
+#include "VideoFXExporter.h"
+#include "Overlay.h"
+
+class testApp : public ofBaseApp{
+
+	public:
+    
+        int w, h;
+    
+        Overlay overlay;
+    
+        ofColor colors[7] = {
+            ofColor::red,
+            ofColor::orange,
+            ofColor::yellow,
+            ofColor::green,
+            ofColor::blue,
+            ofColor::indigo,
+            ofColor::violet
+        };
+        int colorIndex = 0;
+    
+        ofVideoGrabber videoGrabber;
+        ofBaseVideoDraws *videoSource;
+    
+        VideoFX vfx1, vfx2;
+        VideoFX *big;
+    
+		void setup();
+		void update();
+		void draw();
+        void exit();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		
+        VideoFXExporter videoFXExporter;    
+};
