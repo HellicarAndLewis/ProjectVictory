6b8171b28feed242e06b7d536547d0b8eeb55ff7
roxlu <info@roxlu.com>
2013-10-09 12:23:21 +0200

    Bye bye Test_VideoFXStreamer

--- Test_VideoFXStreamer/addons/ofxVideoStreamer/include/x264_config.h
+++
@@ -1,8 +0,0 @@
-#define X264_BIT_DEPTH     8
-#define X264_GPL           1
-#define X264_INTERLACED    1
-#define X264_CHROMA_FORMAT 0
-#define X264_REV 2357
-#define X264_REV_DIFF 0
-#define X264_VERSION " r2357 e61d9f9"
-#define X264_POINTVER "0.138.2357 e61d9f9"
